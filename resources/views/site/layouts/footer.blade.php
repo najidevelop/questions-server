c1790df7d03247d315fcc275740200e93f8c285d
najims2016 <70610331+ahmadms2020@users.noreply.github.com>
2024-07-09 17:44:50 +0200

    Update footer.blade.php

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -27,7 +27,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  // <script src="{{ url('assets/site/js/main.js') }}"></script>
+ 
   
   @yield('js')
    
